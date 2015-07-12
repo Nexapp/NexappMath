730a4d45103c2779dacb60a0d6df65ebcf1f7782
alwal12 <awalsh@nexapp.ca>
2015-07-12 15:30:35 -0400

    Added the Point class

---
+++ src/main/java/ca/nexapp/math/Point.java
@@ -0,0 +1,118 @@
+package ca.nexapp.math;
+
+import java.util.Objects;
+
+public class Point {
+
+    public static final Point ORIGIN = new Point(0.0, 0.0);
+
+    private static final double PRECISION = 0.001;
+
+    private final double x;
+    private final double y;
+
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getDistance() {
+        return Math.hypot(x, y);
+    }
+
+    public Angle getAngle() {
+        if (x == 0 && y == 0) {
+            return Angle.ZERO_DEGREES;
+        } else if (x > 0 && y >= 0) {
+            return Angle.fromRadians(Math.atan(y / x));
+        } else if (x > 0 && y < 0) {
+            return Angle.fromRadians(Math.atan(y / x) + 2.0 * Math.PI);
+        } else if (x < 0) {
+            return Angle.fromRadians(Math.atan(y / x) + Math.PI);
+        } else if (x == 0 && y > 0) {
+            return Angle.fromRadians(Math.PI / 2.0);
+        } else {
+            return Angle.fromRadians(3.0 * Math.PI / 2.0);
+        }
+    }
+
+    public double getDistanceTo(Point otherPoint) {
+        double distanceX = x - otherPoint.x;
+        double distanceY = y - otherPoint.y;
+        return Math.hypot(distanceX, distanceY);
+    }
+
+    public boolean isAtLeftOf(Point other) {
+        return x < other.x;
+    }
+
+    public boolean isAtRightOf(Point other) {
+        return x > other.x;
+    }
+
+    public boolean isAboveOf(Point other) {
+        return y > other.y;
+    }
+
+    public boolean isBelowOf(Point other) {
+        return y < other.y;
+    }
+
+    public Point rotateClockwise(Point pivot, Angle angle) {
+        double sin = Math.sin(angle.toRadians());
+        double cos = Math.cos(angle.toRadians());
+
+        double rotatedX = (x - pivot.x) * cos + (y - pivot.y) * sin + pivot.x;
+        double rotatedY = (x - pivot.x) * -sin + (y - pivot.y) * cos + pivot.y;
+
+        return new Point(rotatedX, rotatedY);
+    }
+
+    public Point rotateCounterClockwise(Point pivot, Angle angle) {
+        double sin = Math.sin(angle.toRadians());
+        double cos = Math.cos(angle.toRadians());
+
+        double rotatedX = (x - pivot.x) * cos - (y - pivot.y) * sin + pivot.x;
+        double rotatedY = (x - pivot.x) * sin + (y - pivot.y) * cos + pivot.y;
+
+        return new Point(rotatedX, rotatedY);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Point)) {
+            return false;
+        }
+
+        Point other = (Point) obj;
+        return Math.abs(x - other.x) < PRECISION && Math.abs(y - other.y) < PRECISION;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + ", " + getY() + ")(" + getDistance() + ", " + getAngle().toDegrees() + ")";
+    }
+
+    public static Point fromCartesian(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point fromPolar(Angle angle, double distance) {
+        double x = distance * Math.cos(angle.toRadians());
+        double y = distance * Math.sin(angle.toRadians());
+        return new Point(x, y);
+    }
+}
---
+++ src/test/java/ca/nexapp/math/PointTest.java
@@ -0,0 +1,204 @@
+package ca.nexapp.math;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PointTest {
+
+    private static final double X = 500.5;
+    private static final double Y = 100.0;
+
+    private static final double DISTANCE = 400.0;
+    private static final Angle ANGLE = Angle.DEGREES_270;
+
+    private static final double TOLERANCE = 0.0001;
+
+    @Test
+    public void givenX_ShouldRetrieveTheSameX() {
+        assertThat(Point.fromCartesian(X, Y).getX()).isWithin(TOLERANCE).of(X);
+    }
+
+    @Test
+    public void givenY_ShouldRetrieveTheSameY() {
+        assertThat(Point.fromCartesian(X, Y).getY()).isWithin(TOLERANCE).of(Y);
+    }
+
+    @Test
+    public void givenADistance_ShouldRetrieveTheSameDistance() {
+        assertThat(Point.fromPolar(ANGLE, DISTANCE).getDistance()).isWithin(TOLERANCE).of(DISTANCE);
+    }
+
+    @Test
+    public void givenAnAngle_ShouldRetrieveTheSameAngle() {
+        assertThat(Point.fromPolar(ANGLE, DISTANCE).getAngle()).isEqualTo(ANGLE);
+    }
+
+    @Test
+    public void givenCartesianPointInFirstQuadrant_ShouldReturnTheEquivalentPolarPoint() {
+        Point cartesianPoint = Point.fromCartesian(1.0, 1.0);
+        Point polarPoint = Point.fromPolar(Angle.fromDegrees(45), 1.4142);
+
+        assertThat(cartesianPoint).isEqualTo(polarPoint);
+    }
+
+    @Test
+    public void givenCartesianPointInSecondQuadrant_ShouldReturnTheEquivalentPolarPoint() {
+        Point cartesianPoint = Point.fromCartesian(-3.0, 10.0);
+        Point polarPoint = Point.fromPolar(Angle.fromDegrees(106.70), 10.4403);
+
+        assertThat(cartesianPoint).isEqualTo(polarPoint);
+    }
+
+    @Test
+    public void givenCartesianPointInThirdQuadrant_ShouldReturnTheEquivalentPolarPoint() {
+        Point cartesianPoint = Point.fromCartesian(-10.0, -15.0);
+        Point polarPoint = Point.fromPolar(Angle.fromDegrees(236.3099), 18.0277);
+
+        assertThat(cartesianPoint).isEqualTo(polarPoint);
+    }
+
+    @Test
+    public void givenCartesianPointInFourthQuadrant_ShouldReturnTheEquivalentPolarPoint() {
+        Point cartesianPoint = Point.fromCartesian(5.0, -8.0);
+        Point polarPoint = Point.fromPolar(Angle.fromDegrees(302.005), 9.43398);
+
+        assertThat(cartesianPoint).isEqualTo(polarPoint);
+    }
+
+    @Test
+    public void givenOrigin_ShouldReturnAnAngleOfZero() {
+        assertThat(Point.ORIGIN.getAngle()).isEqualTo(Angle.ZERO_DEGREES);
+    }
+
+    @Test
+    public void givenCartesianPointAt1_1_ShouldReturnAnAngleOf45Degrees() {
+        assertThat(Point.fromCartesian(1.0, 1.0).getAngle()).isEqualTo(Angle.fromDegrees(45.00));
+    }
+
+    @Test
+    public void givenCartesianPointAt1_Minus1_ShouldReturnAnAngleOf315Degrees() {
+        assertThat(Point.fromCartesian(1.0, -1.0).getAngle()).isEqualTo(Angle.fromDegrees(315.00));
+    }
+
+    @Test
+    public void givenCartesianPointAtMinus5_0_ShouldReturnAnAngleOf180Degrees() {
+        assertThat(Point.fromCartesian(-5.0, 0).getAngle()).isEqualTo(Angle.DEGREES_180);
+    }
+
+    @Test
+    public void givenCartesianPointAt0_6_ShouldReturnAnAngleOf90Degrees() {
+        assertThat(Point.fromCartesian(0, 6.0).getAngle()).isEqualTo(Angle.DEGREES_90);
+    }
+
+    @Test
+    public void givenCartesianPointAt0_Minus7_ShouldReturnAnAngleOf270Degrees() {
+        assertThat(Point.fromCartesian(0, -7.0).getAngle()).isEqualTo(Angle.DEGREES_270);
+    }
+
+    @Test
+    public void givenOriginAndPointAt1_1ThenTheDistanceShouldBe1_4142() {
+        double actualDistance = Point.ORIGIN.getDistanceTo(Point.fromCartesian(1, 1));
+
+        assertThat(actualDistance).isWithin(TOLERANCE).of(1.4142);
+    }
+
+    @Test
+    public void givenSameX_ShouldNotBeAtLeftOfTheOtherPoint() {
+        assertThat(Point.ORIGIN.isAtLeftOf(Point.ORIGIN)).isFalse();
+    }
+
+    @Test
+    public void givenSameX_ShouldNotBeAtRightOfTheOtherPoint() {
+        assertThat(Point.ORIGIN.isAtRightOf(Point.ORIGIN)).isFalse();
+    }
+
+    @Test
+    public void givenXAtLeft_ThenOtherPointShouldBeToTheLeft() {
+        Point otherPoint = Point.fromCartesian(1000, 0);
+
+        assertThat(Point.ORIGIN.isAtLeftOf(otherPoint)).isTrue();
+    }
+
+    @Test
+    public void givenXAtRight_ThenOtherPointShouldNotBeToTheLeft() {
+        Point otherPoint = Point.fromCartesian(1000, 0);
+
+        assertThat(otherPoint.isAtLeftOf(Point.ORIGIN)).isFalse();
+    }
+
+    @Test
+    public void givenXAtRight_ThenOtherPointShouldBeToTheRight() {
+        Point otherPoint = Point.fromCartesian(1000, 0);
+
+        assertThat(otherPoint.isAtRightOf(Point.ORIGIN)).isTrue();
+    }
+
+    @Test
+    public void givenXAtLeft_ThenOtherPointShouldNotBeToTheRight() {
+        Point otherPoint = Point.fromCartesian(1000, 0);
+
+        assertThat(Point.ORIGIN.isAtRightOf(otherPoint)).isFalse();
+    }
+
+    @Test
+    public void givenSameY_ShouldNotBeAboveOfTheOtherPoint() {
+        assertThat(Point.ORIGIN.isAboveOf(Point.ORIGIN)).isFalse();
+    }
+
+    @Test
+    public void givenSameY_ShouldNotBeBelowOfTheOtherPoint() {
+        assertThat(Point.ORIGIN.isBelowOf(Point.ORIGIN)).isFalse();
+    }
+
+    @Test
+    public void givenAboveY_ThenOtherPointShouldBeAbove() {
+        Point otherPoint = Point.fromCartesian(0, 1000);
+
+        assertThat(otherPoint.isAboveOf(Point.ORIGIN)).isTrue();
+    }
+
+    @Test
+    public void givenBelowY_ShouldNotBeAboveOfTheOtherPoint() {
+        Point otherPoint = Point.fromCartesian(0, 1000);
+
+        assertThat(Point.ORIGIN.isAboveOf(otherPoint)).isFalse();
+    }
+
+    @Test
+    public void givenBelowY_ThenOtherPointShouldBeBelow() {
+        Point otherPoint = Point.fromCartesian(0, 1000);
+
+        assertThat(Point.ORIGIN.isBelowOf(otherPoint)).isTrue();
+    }
+
+    @Test
+    public void givenAboveY_ThenOtherPointShouldNotBeBelow() {
+        Point otherPoint = Point.fromCartesian(0, 1000);
+
+        assertThat(otherPoint.isBelowOf(Point.ORIGIN)).isFalse();
+    }
+
+    @Test
+    public void givenAPivotAtX1Y2AndPointAtX2Y4_WhenRotatingClockwiseWith90Degrees_ShouldReturnX3Y1() {
+        Point point = Point.fromCartesian(2, 4);
+        Point pivot = Point.fromCartesian(1, 2);
+
+        Point rotatedPoint = point.rotateClockwise(pivot, Angle.DEGREES_90);
+
+        assertThat(rotatedPoint).isEqualTo(Point.fromCartesian(3, 1));
+    }
+
+    @Test
+    public void givenAPivotAtX2YMinus2AndAPointAtX2Y0_WhenRotatingCounterClockwiseWith90Degrees_ShouldReturnX0YMinus2() {
+        Point point = Point.fromCartesian(2, 0);
+        Point pivot = Point.fromCartesian(2, -2);
+
+        Point rotatedPoint = point.rotateCounterClockwise(pivot, Angle.DEGREES_90);
+
+        assertThat(rotatedPoint).isEqualTo(Point.fromCartesian(0, -2));
+    }
+}
