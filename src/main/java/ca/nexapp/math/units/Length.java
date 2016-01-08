f5a62eec9e1e2dc9440795e4bb1eecfbc021e9b5
awalsh <awalsh@nexapp.ca>
2016-01-08 18:57:14 -0500

    Added a Length class

---
+++ src/main/java/ca/nexapp/math/units/Length.java
@@ -0,0 +1,119 @@
+package ca.nexapp.math.units;
+
+import java.util.Objects;
+
+public class Length implements Comparable<Length> {
+
+    private static final double METERS_IN_A_MILLIMETER = 0.001;
+    private static final double METERS_IN_A_CENTIMETER = 0.01;
+    private static final double METERS_IN_A_DECIMETER = 0.10;
+    private static final double METERS_IN_A_KILOMETER = 1000;
+    private static final double METERS_IN_A_MILE = 1609.34;
+    private static final double METERS_IN_A_YARD = 0.9144;
+    private static final double METERS_IN_A_FOOT = 0.3048;
+    private static final double METERS_IN_AN_INCH = 0.0254;
+
+    private final double meters;
+
+    private Length(double meters) {
+	this.meters = meters;
+    }
+
+    public double toMillimeters() {
+	return meters / METERS_IN_A_MILLIMETER;
+    }
+
+    public double toCentimeters() {
+	return meters / METERS_IN_A_CENTIMETER;
+    }
+
+    public double toDecimeters() {
+	return meters / METERS_IN_A_DECIMETER;
+    }
+
+    public double toMeters() {
+	return meters;
+    }
+
+    public double toKilometers() {
+	return meters / METERS_IN_A_KILOMETER;
+    }
+
+    public double toMiles() {
+	return meters / METERS_IN_A_MILE;
+    }
+
+    public double toYards() {
+	return meters / METERS_IN_A_YARD;
+    }
+
+    public double toInches() {
+	return meters / METERS_IN_AN_INCH;
+    }
+
+    public double toFoot() {
+	return meters / METERS_IN_A_FOOT;
+    }
+
+    public static Length fromMillimeters(double millimeters) {
+	return new Length(millimeters * METERS_IN_A_MILLIMETER);
+    }
+
+    public static Length fromCentimeters(double centimeters) {
+	return new Length(centimeters * METERS_IN_A_CENTIMETER);
+    }
+
+    public static Length fromDecimeters(double decimeters) {
+	return new Length(decimeters * METERS_IN_A_DECIMETER);
+    }
+
+    public static Length fromMeters(double meters) {
+	return new Length(meters);
+    }
+
+    public static Length fromKilometers(double kilometers) {
+	return new Length(kilometers * METERS_IN_A_KILOMETER);
+    }
+
+    public static Length fromMiles(double miles) {
+	return new Length(miles * METERS_IN_A_MILE);
+    }
+
+    public static Length fromYards(double yards) {
+	return new Length(yards * METERS_IN_A_YARD);
+    }
+
+    public static Length fromInches(double inches) {
+	return new Length(inches * METERS_IN_AN_INCH);
+    }
+
+    public static Length fromFoot(double foot) {
+	return new Length(foot * METERS_IN_A_FOOT);
+    }
+
+    @Override
+    public int compareTo(Length other) {
+	return Double.compare(meters, other.meters);
+    }
+
+    @Override
+    public int hashCode() {
+	return Objects.hash(meters);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (!(obj instanceof Length)) {
+	    return false;
+	}
+
+	Length other = (Length) obj;
+	return Objects.equals(meters, other.meters);
+    }
+
+    @Override
+    public String toString() {
+	return meters + "m";
+    }
+
+}
---
+++ src/test/java/ca/nexapp/math/units/LengthTest.java
@@ -0,0 +1,153 @@
+package ca.nexapp.math.units;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+public class LengthTest {
+
+    private static final double TOLERANCE = 0.00001;
+
+    @Test
+    public void convertingMetersToMeters_ShouldReturnTheSameValue() {
+	Length length = Length.fromMeters(15);
+
+	assertThat(length.toMeters()).isWithin(TOLERANCE).of(15);
+    }
+
+    @Test
+    public void canConvertMetersToKilometers() {
+	Length length = Length.fromMeters(2500);
+
+	assertThat(length.toKilometers()).isWithin(TOLERANCE).of(2.5);
+    }
+
+    @Test
+    public void convertingKilometersToKilometers_ShouldReturnTheSameValue() {
+	Length length = Length.fromKilometers(3);
+
+	assertThat(length.toKilometers()).isWithin(TOLERANCE).of(3);
+    }
+
+    @Test
+    public void canConvertMetersToDecimeters() {
+	Length length = Length.fromMeters(10);
+
+	assertThat(length.toDecimeters()).isWithin(TOLERANCE).of(100);
+    }
+
+    @Test
+    public void convertingDecimetersToDecimeters_ShouldReturnTheSameValue() {
+	Length length = Length.fromDecimeters(85);
+
+	assertThat(length.toDecimeters()).isWithin(TOLERANCE).of(85);
+    }
+
+    @Test
+    public void canConvertDecimetersToCentimeters() {
+	Length length = Length.fromDecimeters(25);
+
+	assertThat(length.toCentimeters()).isWithin(TOLERANCE).of(250);
+    }
+
+    @Test
+    public void convertingCentimetersToCentimeters_ShouldReturnTheSameValue() {
+	Length length = Length.fromCentimeters(45);
+
+	assertThat(length.toCentimeters()).isWithin(TOLERANCE).of(45);
+    }
+
+    @Test
+    public void canConvertCentimetersToMillimeters() {
+	Length length = Length.fromCentimeters(34);
+
+	assertThat(length.toMillimeters()).isWithin(TOLERANCE).of(340);
+    }
+
+    @Test
+    public void convertingMillimetersToMillimeters_ShouldReturnTheSameValue() {
+	Length length = Length.fromMillimeters(73);
+
+	assertThat(length.toMillimeters()).isWithin(TOLERANCE).of(73);
+    }
+
+    @Test
+    public void canConvertKilometersToMiles() {
+	Length length = Length.fromKilometers(100);
+
+	assertThat(length.toMiles()).isWithin(TOLERANCE).of(62.137273);
+    }
+
+    @Test
+    public void convertingMilesToMiles_ShouldReturnTheSameValue() {
+	Length length = Length.fromMiles(87);
+
+	assertThat(length.toMiles()).isWithin(TOLERANCE).of(87);
+    }
+
+    @Test
+    public void canConvertMetersToYards() {
+	Length length = Length.fromMeters(100);
+
+	assertThat(length.toYards()).isWithin(TOLERANCE).of(109.36132);
+    }
+
+    @Test
+    public void convertingYardsToYards_ShouldReturnTheSameValue() {
+	Length length = Length.fromYards(54);
+
+	assertThat(length.toYards()).isWithin(TOLERANCE).of(54);
+    }
+
+    @Test
+    public void canConvertMetersToInches() {
+	Length length = Length.fromMeters(1.65);
+
+	assertThat(length.toInches()).isWithin(TOLERANCE).of(64.96063);
+    }
+
+    @Test
+    public void convertingInchesToInches_ShouldReturnTheSameValue() {
+	Length length = Length.fromInches(900);
+
+	assertThat(length.toInches()).isWithin(TOLERANCE).of(900);
+    }
+
+    @Test
+    public void canConvertMetersToFoot() {
+	Length length = Length.fromMeters(2.07);
+
+	assertThat(length.toFoot()).isWithin(TOLERANCE).of(6.791339);
+    }
+
+    @Test
+    public void convertingFootToFoot_ShouldReturnTheSameValue() {
+	Length length = Length.fromFoot(42);
+
+	assertThat(length.toFoot()).isWithin(TOLERANCE).of(42);
+    }
+
+    @Test
+    public void aSmallerLength_ShouldBeLesser() {
+	Length aSmallLength = Length.fromMiles(16);
+	Length aLongLength = Length.fromMiles(875);
+
+	assertThat(aSmallLength).isLessThan(aLongLength);
+    }
+
+    @Test
+    public void theSameLength_ShouldBeComparativelyEquals() {
+	Length length = Length.fromCentimeters(43);
+
+	assertThat(length).isEquivalentAccordingToCompareTo(length);
+    }
+
+    @Test
+    public void aLongerLength_ShouldBeGreater() {
+	Length aSmallLength = Length.fromMiles(16);
+	Length aLongLength = Length.fromMiles(875);
+
+	assertThat(aLongLength).isGreaterThan(aSmallLength);
+    }
+
+}
