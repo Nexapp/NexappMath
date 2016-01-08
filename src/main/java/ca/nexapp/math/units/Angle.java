739eb2afb5dc228dbb94c6734d9c6f60b17ae9e7
awalsh <awalsh@nexapp.ca>
2016-01-08 18:57:55 -0500

    Moved static constructor before hashCode/equals methods

--- src/main/java/ca/nexapp/math/units/Angle.java
+++ src/main/java/ca/nexapp/math/units/Angle.java
@@ -13,92 +13,93 @@ public class Angle {
     private final double angleInDegrees;
 
     private Angle(double angleInDegrees) {
-        this.angleInDegrees = angleInDegrees;
+	this.angleInDegrees = angleInDegrees;
     }
 
     public double toDegrees() {
-        return angleInDegrees;
+	return angleInDegrees;
     }
 
     public double toRadians() {
-        return Math.toRadians(angleInDegrees);
+	return Math.toRadians(angleInDegrees);
     }
 
     public Angle invert() {
-        return new Angle(-angleInDegrees);
+	return new Angle(-angleInDegrees);
     }
 
     public Angle normalize() {
-        double c = angleInDegrees % 360;
-        double normalizedAngleInDegrees = c >= 0 ? c : 360.0 + c;
-        return new Angle(normalizedAngleInDegrees);
+	double c = angleInDegrees % 360;
+	double normalizedAngleInDegrees = c >= 0 ? c : 360.0 + c;
+	return new Angle(normalizedAngleInDegrees);
     }
 
     public boolean isInFirstQuadrant() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 0 < normalizedAngleInDegrees && normalizedAngleInDegrees < 90;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 0 < normalizedAngleInDegrees && normalizedAngleInDegrees < 90;
     }
 
     public boolean isInFirstQuadrantOrAxes() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 0 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 90;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 0 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 90;
     }
 
     public boolean isInSecondQuadrant() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 90 < normalizedAngleInDegrees && normalizedAngleInDegrees < 180;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 90 < normalizedAngleInDegrees && normalizedAngleInDegrees < 180;
     }
 
     public boolean isInSecondQuadrantOrAxes() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 90 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 180;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 90 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 180;
     }
 
     public boolean isInThirdQuadrant() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 180 < normalizedAngleInDegrees && normalizedAngleInDegrees < 270;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 180 < normalizedAngleInDegrees && normalizedAngleInDegrees < 270;
     }
 
     public boolean isInThirdQuadrantOrAxes() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 180 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 270;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 180 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 270;
     }
 
     public boolean isInFourthQuadrant() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 270 < normalizedAngleInDegrees && normalizedAngleInDegrees < 360;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 270 < normalizedAngleInDegrees && normalizedAngleInDegrees < 360;
     }
 
     public boolean isInFourthQuadrantOrAxes() {
-        double normalizedAngleInDegrees = normalize().toDegrees();
-        return 270 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 360 || normalizedAngleInDegrees == 0.0;
+	double normalizedAngleInDegrees = normalize().toDegrees();
+	return 270 <= normalizedAngleInDegrees && normalizedAngleInDegrees <= 360 || normalizedAngleInDegrees == 0.0;
+    }
+
+    public static Angle fromRadians(double angleInRadians) {
+	return new Angle(Math.toDegrees(angleInRadians));
+    }
+
+    public static Angle fromDegrees(double angleInDegrees) {
+	return new Angle(angleInDegrees);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(angleInDegrees);
+	return Objects.hash(angleInDegrees);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (!(obj instanceof Angle)) {
-            return false;
-        }
+	if (!(obj instanceof Angle)) {
+	    return false;
+	}
 
-        Angle other = (Angle) obj;
-        return Objects.equals(angleInDegrees, other.angleInDegrees);
+	Angle other = (Angle) obj;
+	return Objects.equals(angleInDegrees, other.angleInDegrees);
     }
 
     @Override
     public String toString() {
-        return "[Degrees: " + toDegrees() + ", Radians: " + toRadians() + "]";
+	return "[Degrees: " + toDegrees() + ", Radians: " + toRadians() + "]";
     }
 
-    public static Angle fromRadians(double angleInRadians) {
-        return new Angle(Math.toDegrees(angleInRadians));
-    }
-
-    public static Angle fromDegrees(double angleInDegrees) {
-        return new Angle(angleInDegrees);
-    }
 }
