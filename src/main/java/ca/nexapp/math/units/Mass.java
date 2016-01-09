f1b432a90676203adcb42be592398417cfc139a7
awalsh <awalsh@nexapp.ca>
2016-01-08 19:24:20 -0500

    Added a Mass class.

---
+++ src/main/java/ca/nexapp/math/units/Mass.java
@@ -0,0 +1,83 @@
+package ca.nexapp.math.units;
+
+import java.util.Objects;
+
+public class Mass implements Comparable<Mass> {
+
+    private static final double GRAMS_IN_AN_OUNCE = 28.3495;
+    private static final double GRAMS_IN_A_POUND = 453.592;
+    private static final double GRAMS_IN_A_KILOGRAM = 1000;
+    private static final double GRAMS_IN_A_TONNE = GRAMS_IN_A_KILOGRAM * 1000;
+
+    private final double grams;
+
+    private Mass(double grams) {
+	this.grams = grams;
+    }
+
+    public double toOunces() {
+	return grams / GRAMS_IN_AN_OUNCE;
+    }
+
+    public double toPounds() {
+	return grams / GRAMS_IN_A_POUND;
+    }
+
+    public double toGrams() {
+	return grams;
+    }
+
+    public double toKilograms() {
+	return grams / GRAMS_IN_A_KILOGRAM;
+    }
+
+    public double toTonnes() {
+	return grams / GRAMS_IN_A_TONNE;
+    }
+
+    public static Mass ounces(double ounces) {
+	return new Mass(ounces * GRAMS_IN_AN_OUNCE);
+    }
+
+    public static Mass pounds(double pounds) {
+	return new Mass(pounds * GRAMS_IN_A_POUND);
+    }
+
+    public static Mass grams(double grams) {
+	return new Mass(grams);
+    }
+
+    public static Mass kilograms(double kilograms) {
+	return new Mass(kilograms * GRAMS_IN_A_KILOGRAM);
+    }
+
+    public static Mass tonnes(double tonnes) {
+	return new Mass(tonnes * GRAMS_IN_A_TONNE);
+    }
+
+    @Override
+    public int compareTo(Mass other) {
+	return Double.compare(grams, other.grams);
+    }
+
+    @Override
+    public int hashCode() {
+	return Objects.hash(grams);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (!(obj instanceof Mass)) {
+	    return false;
+	}
+
+	Mass other = (Mass) obj;
+	return Objects.equals(grams, other.grams);
+    }
+
+    @Override
+    public String toString() {
+	return grams + "g";
+    }
+
+}
---
+++ src/test/java/ca/nexapp/math/units/MassTest.java
@@ -0,0 +1,97 @@
+package ca.nexapp.math.units;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+public class MassTest {
+
+    private static final double TOLERANCE = 0.0001;
+
+    @Test
+    public void convertingTonnesToTonnes_ShouldReturnTheSameValue() {
+	Mass mass = Mass.tonnes(5);
+
+	assertThat(mass.toTonnes()).isWithin(TOLERANCE).of(5);
+    }
+
+    @Test
+    public void canConvertTonnesToKilograms() {
+	Mass mass = Mass.tonnes(2);
+
+	assertThat(mass.toKilograms()).isWithin(TOLERANCE).of(2000);
+    }
+
+    @Test
+    public void convertingKilogramsToKilograms_ShouldReturnTheSameValue() {
+	Mass mass = Mass.kilograms(1.8);
+
+	assertThat(mass.toKilograms()).isWithin(TOLERANCE).of(1.8);
+    }
+
+    @Test
+    public void canConvertKilogramsToGrams() {
+	Mass mass = Mass.kilograms(5);
+
+	assertThat(mass.toGrams()).isWithin(TOLERANCE).of(5000);
+    }
+
+    @Test
+    public void convertingGramsToGrams_ShouldReturnTheSameValue() {
+	Mass mass = Mass.grams(50);
+
+	assertThat(mass.toGrams()).isWithin(TOLERANCE).of(50);
+    }
+
+    @Test
+    public void canConvertGramsToPounds() {
+	Mass mass = Mass.grams(100);
+
+	assertThat(mass.toPounds()).isWithin(TOLERANCE).of(0.2204);
+    }
+
+    @Test
+    public void convertingPoundsToPounds_ShouldReturnTheSameValue() {
+	Mass mass = Mass.pounds(45);
+
+	assertThat(mass.toPounds()).isWithin(TOLERANCE).of(45);
+    }
+
+    @Test
+    public void canConvertPoundsToGrams() {
+	Mass mass = Mass.pounds(1);
+
+	assertThat(mass.toGrams()).isWithin(TOLERANCE).of(453.592);
+    }
+
+    @Test
+    public void convertingOuncesToOunces_ShouldReturnTheSameValue() {
+	Mass mass = Mass.ounces(34);
+
+	assertThat(mass.toOunces()).isWithin(TOLERANCE).of(34);
+    }
+
+    @Test
+    public void aSmallMass_ShouldBeLesser() {
+	Mass smallMass = Mass.grams(20);
+	Mass highMass = Mass.grams(900);
+
+	assertThat(smallMass).isLessThan(highMass);
+    }
+
+    @Test
+    public void theSameMass_ShouldBeEqualsComparatively() {
+	Percentage percentage = Percentage.fromRatio(43);
+
+	assertThat(percentage).isEquivalentAccordingToCompareTo(percentage);
+    }
+
+    @Test
+    public void aHighMass_ShouldBeGreater() {
+	Mass smallMass = Mass.grams(20);
+	Mass highMass = Mass.grams(900);
+
+	assertThat(highMass).isGreaterThan(smallMass);
+    }
+
+}
